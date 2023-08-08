0b4460bffc5f71f3e2e947606ca1a8034961135a
Ekaterina Lubyankina <katylubyankina@gmail.com>
2023-08-08 13:21:26 +0300

    function for cutting audio file added

--- src/audio/audio.py
+++ src/audio/audio.py
@@ -1,13 +1,45 @@
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 from pytube import YouTube, exceptions
 
+import src.config as config
 
-def download_audio(link: str) -> dict:
-    yt = YouTube(link)
+
+def download_audio(url: str) -> str:
+    """Downloads audio track from video
+
+    Downloads audio from video clip only from Youtube to /src/tracks directory.
+
+    Args:
+        url (str): url for video (Youtube only)
+
+    Returns:
+        Path to downloaded audio track as string
+    """
+    yt = YouTube(url)
     title = yt.title
     yt = yt.streams.get_audio_only()
-    path = f"src/video/{title}.mp4"
+    path = f"src/tracks/{title}.mp4"
     try:
-        yt.download(output_path="src/video/")
+        yt.download(output_path="src/tracks/")
     except exceptions.PytubeError("Unable to download a video."):
         path = None
-    return {"path": path}
+    return path
+
+
+def cut_audio(path: str) -> str:
+    """Cuts audio track.
+
+    Function returns only first minutes of audio file.
+    Duration of cropped audio is specified in config file.
+    New file name is 'crop_{audio_filename}.mp4'
+
+    Args:
+        path (str): path to audio
+
+    Returns:
+        Path to cropped audio file
+    """
+    end_time = config.get_settings().end_of_audio_time
+    cut_title = "src/tracks/" + "crop_" + path.split("/")[-1]
+    ffmpeg_extract_subclip(path, 0, end_time, targetname=cut_title)
+    return cut_title
